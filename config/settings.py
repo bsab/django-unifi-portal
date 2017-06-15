277ecdf32bc27c5834b230ef6f1a594becb8f4b7
sab <tino.saba@gmail.com>
2017-06-15 17:19:49 +0200

    moved template inside unifi_portal dir

--- config/settings.py
+++ config/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'unifi_portal/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
